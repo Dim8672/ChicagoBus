0fa5ba680b2c792b1aacdbabc46c9713c9818ba6
Dim8672 <dimitri.mella@gmail.com>
2017-04-12 13:57:30 +0200

    Mise à jour des fichiers du projet

--- src/main/java/ch/hearc/ig/asi/iam/presentation/bean/MapMarkers.java
+++ src/main/java/ch/hearc/ig/asi/iam/presentation/bean/MapMarkers.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.hearc.ig.asi.iam.presentation.bean;
 
 import java.io.Serializable;
